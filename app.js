a81fb83c106204a252d60366412009d22bd0308b
EBEREGIT <samsoneberenjoku@gmail.com>
2020-04-27 20:16:10 +0100

    retrieve image API completed

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
 const cloudinary = require("cloudinary").v2;
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const db = require('./services/dbConnect.js');
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const db = require("./services/dbConnect.js");
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+  api_secret: process.env.API_SECRET,
 });
 
 app.get("/", (request, response, next) => {
@@ -23,19 +23,21 @@ app.get("/", (request, response, next) => {
 
 // image upload API
 app.post("/image-upload", (request, response) => {
-    // collected image from a user
-    const data = {
-      image: request.body.image,
-    }
+  // collected image from a user
+  const data = {
+    image: request.body.image,
+  };
 
-    // upload image here
-    cloudinary.uploader.upload(data.image)
+  // upload image here
+  cloudinary.uploader
+    .upload(data.image)
     .then((result) => {
       response.status(200).send({
         message: "success",
         result,
       });
-    }).catch((error) => {
+    })
+    .catch((error) => {
       response.status(500).send({
         message: "failure",
         error,
@@ -48,46 +50,84 @@ app.post("/persist-image", (request, response) => {
   // collected image from a user
   const data = {
     title: request.body.title,
-    image: request.body.image
-  }
+    image: request.body.image,
+  };
 
   // upload image here
-  cloudinary.uploader.upload(data.image)
-  .then((image) => {
-    db.pool.connect((err, client) => {
-      // inset query to run if the upload to cloudinary is successful
-      const insertQuery = 'INSERT INTO images (title, cloudinary_id, image_url) VALUES($1,$2,$3) RETURNING *';
-      const values = [data.title, image.public_id, image.secure_url];
+  cloudinary.uploader
+    .upload(data.image)
+    .then((image) => {
+      db.pool.connect((err, client) => {
+        // inset query to run if the upload to cloudinary is successful
+        const insertQuery =
+          "INSERT INTO images (title, cloudinary_id, image_url) VALUES($1,$2,$3) RETURNING *";
+        const values = [data.title, image.public_id, image.secure_url];
 
-      // execute query
-      client.query(insertQuery, values)
-      .then((result) => {
-        result = result.rows[0];
+        // execute query
+        client
+          .query(insertQuery, values)
+          .then((result) => {
+            result = result.rows[0];
 
-        // send success response
-        response.status(201).send({
-          status: "success",
-          data: {
-            message: "Image Uploaded Successfully",
-            title: result.title,
-            image_url: result.image_url,
-          },
-        })
-      }).catch((e) => {
-        response.status(500).send({
-          message: "failure",
-          e,
-        });
-      })
-    })  
-  }).catch((error) => {
-    response.status(500).send({
-      message: "failure",
-      error,
+            // send success response
+            response.status(201).send({
+              status: "success",
+              data: {
+                message: "Image Uploaded Successfully",
+                title: result.title,
+                cloudinary_id: result.cloudinary_id,
+                image_url: result.image_url,
+              },
+            });
+          })
+          .catch((e) => {
+            response.status(500).send({
+              message: "failure",
+              e,
+            });
+          });
+      });
+    })
+    .catch((error) => {
+      response.status(500).send({
+        message: "failure",
+        error,
+      });
     });
-  });
 });
 
+app.get("/retrieve-image/:cloudinary_id", (request, response) => {
+  // data from user
+  const { cloudinary_id } = request.params;
+  
+  db.pool.connect((err, client) => {
+    // query to find image
+    const query = "SELECT * FROM images WHERE cloudinary_id = $1";
+    const value = [cloudinary_id];
 
+    // execute query
+    client
+      .query(query, value)
+      .then((output) => {
+        response.status(200).send({
+          status: "success",
+          data: {
+            id: output.rows[0].cloudinary_id,
+            title: output.rows[0].title,
+            url: output.rows[0].image_url,
+          },
+        });
+      })
+      .catch((error) => {
+        response.status(401).send({
+          status: "failure",
+          data: {
+            message: "could not retrieve record!",
+            error,
+          },
+        });
+      });
+  });
+});
 
 module.exports = app;
