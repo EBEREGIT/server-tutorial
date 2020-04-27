dd22ba9dd1170efa63a9d6155e27510e166a693f
EBEREGIT <samsoneberenjoku@gmail.com>
2020-04-27 10:38:16 +0100

    create db connection file and created table

---
+++ services/dbConnect.js
@@ -0,0 +1,48 @@
+const pg = require("pg");
+
+const config = {
+  user: "tutorial",
+  database: "tutorial",
+  password: "tutorial",
+  port: 5432,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+pool.on("connect", () => {
+  console.log("connected to the Database");
+});
+
+const createTables = () => {
+  const imageTable = `CREATE TABLE IF NOT EXISTS
+    images(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(128) NOT NULL,
+      cloudinary_id VARCHAR(128) NOT NULL
+    )`;
+  pool
+    .query(imageTable)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+pool.on("remove", () => {
+  console.log("client removed");
+  process.exit(0);
+});
+
+//export pool and createTables to be accessible  from an where within the application
+module.exports = {
+  createTables,
+  pool,
+};
+
+require("make-runnable");
