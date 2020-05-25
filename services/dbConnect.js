2fa6f8c241d208e4385b2eee81f2e801ea50b1cb
EBEREGIT <samsoneberenjoku@gmail.com>
2020-05-25 15:04:09 +0100

    refactored the dbConnect and .env file to fit in to heroku; Added the database SQL file

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,19 +1,24 @@
-const pg = require("pg");
+const pg = require('pg');
+require('dotenv').config();
 
-const config = {
-  user: "tutorial",
-  database: "tutorial",
-  password: "tutorial",
-  port: 5432,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000,
-};
+// set production variable. This will be called when deployed to a live host
+const isProduction = process.env.NODE_ENV === 'production';
 
-const pool = new pg.Pool(config);
+// configuration details
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-pool.on("connect", () => {
-  console.log("connected to the Database");
+// if project has been deployed, connect with the host's DATABASE_URL
+// else connect with the local DATABASE_URL
+const pool = new pg.Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 });
+
+// display message on success message if successful
+pool.on('connect', () => {
+  console.log('Teamwork Database connected successfully!');
+});
+
 
 const createTables = () => {
   const imageTable = `CREATE TABLE IF NOT EXISTS
---
+++ services/tutorial.sql
@@ -0,0 +1,107 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.1
+-- Dumped by pg_dump version 12.1
+
+-- Started on 2020-05-25 14:27:49
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 203 (class 1259 OID 49321)
+-- Name: images; Type: TABLE; Schema: public; Owner: tutorial
+--
+
+CREATE TABLE public.images (
+    id integer NOT NULL,
+    title character varying(128) NOT NULL,
+    cloudinary_id character varying(128) NOT NULL,
+    image_url character varying(128) NOT NULL
+);
+
+
+ALTER TABLE public.images OWNER TO tutorial;
+
+--
+-- TOC entry 202 (class 1259 OID 49319)
+-- Name: images_id_seq; Type: SEQUENCE; Schema: public; Owner: tutorial
+--
+
+CREATE SEQUENCE public.images_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.images_id_seq OWNER TO tutorial;
+
+--
+-- TOC entry 2823 (class 0 OID 0)
+-- Dependencies: 202
+-- Name: images_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tutorial
+--
+
+ALTER SEQUENCE public.images_id_seq OWNED BY public.images.id;
+
+
+--
+-- TOC entry 2687 (class 2604 OID 49324)
+-- Name: images id; Type: DEFAULT; Schema: public; Owner: tutorial
+--
+
+ALTER TABLE ONLY public.images ALTER COLUMN id SET DEFAULT nextval('public.images_id_seq'::regclass);
+
+
+--
+-- TOC entry 2817 (class 0 OID 49321)
+-- Dependencies: 203
+-- Data for Name: images; Type: TABLE DATA; Schema: public; Owner: tutorial
+--
+
+COPY public.images (id, title, cloudinary_id, image_url) FROM stdin;
+3	Updated Image	yhzoolmc7o131ylh4nsq	https://res.cloudinary.com/dunksyqjj/image/upload/v1589211420/yhzoolmc7o131ylh4nsq.jpg
+\.
+
+
+--
+-- TOC entry 2824 (class 0 OID 0)
+-- Dependencies: 202
+-- Name: images_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tutorial
+--
+
+SELECT pg_catalog.setval('public.images_id_seq', 7, true);
+
+
+--
+-- TOC entry 2689 (class 2606 OID 49326)
+-- Name: images images_pkey; Type: CONSTRAINT; Schema: public; Owner: tutorial
+--
+
+ALTER TABLE ONLY public.images
+    ADD CONSTRAINT images_pkey PRIMARY KEY (id);
+
+
+-- Completed on 2020-05-25 14:27:50
+
+--
+-- PostgreSQL database dump complete
+--
+
