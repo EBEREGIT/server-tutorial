8a9516dfdeb661a7ec4027a35d0040f5ce9ed797
EBEREGIT <samsoneberenjoku@gmail.com>
2020-05-26 09:31:54 +0100

    db issues

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 // configuration details
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?ssl = true`;
 
 // if project has been deployed, connect with the host's DATABASE_URL
 // else connect with the local DATABASE_URL
